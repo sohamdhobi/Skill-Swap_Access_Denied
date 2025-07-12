c479718bb306dcda9a4bd87595869620f14f2668
sohamdhobi <sohamdhobi7@gmail.com>
2025-07-12 10:15:33 +0530

    deleted

--- skillswap_backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, SkillViewSet, SwapRequestViewSet
-
-router = DefaultRouter()
-router.register(r'profiles', UserProfileViewSet)
-router.register(r'skills', SkillViewSet)
-router.register(r'swaps', SwapRequestViewSet)
-
-urlpatterns = router.urls
